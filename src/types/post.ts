17df2b3982e4f7beee1d2dad21aee3a3d8680148
Maksym Radchuk <maxr96@gmail.com>
2022-11-26 15:52:17 +0100

    ✨ Complete the main page.

---
+++ src/types/post.ts
@@ -0,0 +1,11 @@
+type PostType = {
+    slug?: string
+    title: string
+    date: string
+    coverImage: string
+    excerpt: string
+    content: string,
+    tags: string
+}
+
+export default PostType
