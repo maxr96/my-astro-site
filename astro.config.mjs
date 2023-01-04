747fa1567d9f655e03a0793d00b32c5806a24318
Maksym Radchuk <maxr96@gmail.com>
2023-01-04 23:29:04 +0100

    ✨ Add Astro integration to generate robots file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
-import prefetch from "@astrojs/prefetch";
+import prefetch from '@astrojs/prefetch'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), prefetch()]
-});
+  site: 'https://my-astro-site-flame.vercel.app',
+  integrations: [mdx(), sitemap(), tailwind(), prefetch(), robotsTxt()]
+})
