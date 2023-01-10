a5e5efe06b6a24989aebf991c845c5a24ac9e1d7
Maksym Radchuk <maxr96@gmail.com>
2023-01-10 21:22:41 +0100

    🔍 Serve correct title and images in meta tags.

--- src/types/post.ts
+++ src/types/post.ts
@@ -1,11 +1,11 @@
 type PostType = {
-    slug?: string
-    title: string
-    date: string
-    coverImage: string
-    excerpt: string
-    content: string,
-    tags: string
+  slug?: string
+  title: string
+  pubDate: string
+  coverImage: string
+  excerpt: string
+  content: string
+  tags: string
 }
 
 export default PostType
